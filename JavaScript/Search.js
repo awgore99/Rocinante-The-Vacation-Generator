766bdac863199a56918121c8ccc1923c6f600a3b
Paris Bland <paris@pariselectra.com>
2022-10-13 10:49:40 -0500

    resync

--- JavaScript/Search.js
+++ JavaScript/Search.js
@@ -77,14 +77,14 @@ function searchFunction(data){
 
 
 
-//Assign variables to IDs in selections.html
+//Assign variables to IDs in selections.html 
 //fetch and .then API's to search for information, taking in the aray of starting and endinglocations from search.js
 //      populate the leftmost container with flight options, drive cost, and train cost
 //          create a dropdown <select> menu for the user to select an option from one of these
 //      populate the center container with hotel and airbnb information
 //          create another dropdown <select> menu for the user to select housing accomidations
 //      populate the rightmost container with information on things to do in the area
-//          either a dropdown menu or checkboxes for the user to potentialy select multiple options
+//          either a dropdown menu or checkboxes for the user to potentialy select multiple options.
 //
 //take in all selections from the user into an array, using a "submit" button that also relocates the user to the final "results.html" page
 
